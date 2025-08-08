820a24c8a21133ec0a724d0651ef722b0bbadf7f
onyedkichi23 <ozohenrywk@gmail.com>
2025-08-08 22:28:59 +0100

    feat(utils): Add cn util

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export const cn = (...inputs: ClassValue[]) => {
+	return twMerge(clsx(inputs));
+};
